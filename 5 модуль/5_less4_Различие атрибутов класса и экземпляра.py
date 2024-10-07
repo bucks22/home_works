13dd40a7884de4c14a29d85eb7d160c4bcab3c97
bucks22 <121617528+bucks22@users.noreply.github.com>
2024-10-07 17:13:34 +0500

    5_less4.py

---
+++ 5 модуль/5_less4_Различие атрибутов класса и экземпляра.py
@@ -0,0 +1,24 @@
+class House:
+    houses_history = []
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
+    def __init__(self, name, fn):
+        self.name = name
+        self.fn = int(fn)
+    def __del__(self):
+        return print(f'{self.name} снесен, но он останется в истории')
+
+
+
+dom1 = House('Жк1', 12)
+print(House.houses_history)
+dom2 = House('Жк2', 32)
+print(House.houses_history)
+dom3 = House('Жк3', 42)
+print(House.houses_history)
+
+del dom2
+del dom3
+print(House.houses_history)
