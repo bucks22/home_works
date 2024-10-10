2b6fd59ae688f53c314983cc1e8e476580f61eb4
bucks22 <121617528+bucks22@users.noreply.github.com>
2024-10-10 22:28:17 +0500

    7_less2.py

--- 7 модуль_Работа с файлами и форматированный вывод/2. Позиционирование в файле/7_less2_Позиционирование в файле.py
+++ 7 модуль_Работа с файлами и форматированный вывод/2. Позиционирование в файле/7_less2_Позиционирование в файле.py
@@ -1,15 +1,24 @@
 def custom_write(file_name, strings):
-    file = open(file_name, 'a', encoding = 'utf-8')
+    file = open(file_name, 'a', encoding='utf-8')
+    strings_positions = {}
+    start_position = 0
 
-    file.write(list(strings) + '\n')
+    for line_number, string in enumerate(strings, 1):
+        byte_position = start_position
+        file.write(string + '\n')
+        start_position += len(string.encode('utf-8')) + 2
+        strings_positions[(line_number, byte_position)] = string
+    file.close()
+    return strings_positions
+
 
 info = [
     'Text for tell.',
     'Используйте кодировку utf-8.',
     'Because there are 2 languages!',
     'Спасибо!'
-    ]
+]
 
 result = custom_write('test.txt', info)
 for elem in result.items():
-  print(elem)
+    print(elem)
