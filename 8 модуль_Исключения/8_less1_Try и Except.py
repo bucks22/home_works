d735b62a3224b4ea345d5684acfebc7fc13b2e24
bucks22 <121617528+bucks22@users.noreply.github.com>
2024-10-13 18:37:51 +0500

    8_less1.py

---
+++ 8 модуль_Исключения/8_less1_Try и Except.py
@@ -0,0 +1,14 @@
+def add_everything_up(a, b):
+    if type(a) != bool and isinstance(b, (float, str, int)):
+        try:
+            c = a + b
+        except TypeError:
+            c = str(a) + str(b)
+        return c
+    else:
+        print('Введите числа или строки')
+    return None
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
