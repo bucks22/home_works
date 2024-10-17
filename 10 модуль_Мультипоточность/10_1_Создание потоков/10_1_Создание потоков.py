42d29930115d3daab763df549bdd8f07d40283e8
bucks22 <121617528+bucks22@users.noreply.github.com>
2024-10-17 19:24:09 +0500

    10_less1.py

---
+++ 10 модуль_Мультипоточность/10_1_Создание потоков/10_1_Создание потоков.py
@@ -0,0 +1,40 @@
+import time
+from time import sleep
+from datetime import datetime
+from threading import Thread
+
+
+time_start = datetime.now()
+def write_words(word_count, file_name):
+    for i in range(1, word_count + 1):
+        with open(file_name, 'a', encoding='utf-8') as file:
+            file.write(f'Какое-то слово № {i}' + '\n')
+            time.sleep(0.1)
+    return f'Завершилась запись в файл {file_name}'
+
+
+
+thr_1 = Thread(target=write_words, args=(10, 'example5.txt'))
+thr_2 = Thread(target=write_words, args=(30, 'example6.txt'))
+thr_3 = Thread(target=write_words, args=(200, 'example7.txt'))
+thr_4 = Thread(target=write_words, args=(100, 'example8.txt'))
+
+thr_1.start()
+thr_2.start()
+thr_3.start()
+thr_4.start()
+
+thr_1.join()
+thr_2.join()
+thr_3.join()
+thr_4.join()
+
+
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+
+time_finish = datetime.now()
+time_res = time_finish - time_start
+print(time_res)
