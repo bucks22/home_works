c3a62c95b9899dab1f8b068b2d7d829d350f19c0
bucks22 <121617528+bucks22@users.noreply.github.com>
2024-12-20 15:29:54 +0500

    14_less2.py

---
+++ 14 модуль_Библиотека для работы с данными/14_2_14_2_Выбор элементов и функции в SQL запросах/14_2_Выбор элементов и функции в SQL запросах.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+"""
+               )
+cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users(email)")
+
+# for i in range(1, 11):
+#     cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", (f'User{i}',
+#                                                                                              f'email{i}@gmail.com',
+#                                                                                              i*10, 1000))
+
+# for i in range (1, 11, 2):
+#     cursor.execute(f"UPDATE Users SET balance=? WHERE id=?", (500, f'{i}'))
+#
+# for i in range (1, 11, 3):
+#     cursor.execute("DELETE FROM Users WHERE id = ?", (f'{i}',))
+#
+# cursor.execute("SELECT * FROM Users WHERE age != 60")
+# users = cursor.fetchall()
+# for i in users:
+#     print(f"Имя: {i[1]} | Почта: {i[2]} | Возраст: {i[3]} | Баланс: {i[4]}")
+
+cursor.execute("DELETE FROM Users WHERE id = 6")
+cursor.execute("SELECT COUNT(*) FROM Users")
+total = cursor.fetchone()[0]
+cursor.execute("SELECT SUM(balance) FROM Users")
+sum = cursor.fetchone()[0]
+
+print(sum / total)
+
+connection.commit()
+connection.close()
